4998384a1a021a50c8ffb229804a82cdc970758c
chitresh97 <chitu1997@gmail.com>
2018-11-09 08:59:56 +0530

    post order traversal

--- main.cpp
+++ main.cpp
@@ -4,6 +4,15 @@
 #include<queue>
 using namespace std;
 
+void postOrder(TreeNode<int>* root) {
+
+  for(int i=0;i<root->children.size();i++){
+    postOrder(root->children[i]);
+  }
+  cout<<root->data<<" ";
+
+}
+
 int numLeafNodes(TreeNode<int>* root) {
   if(root->children.size()==0)
     return 1;
